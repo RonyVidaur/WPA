c63038e2e6b88786a27cb3303f577c894c38a7fd
Rony Vidaur <rony.vidaur@gmail.com>
2017-04-16 14:30:04 -0600

    Dummy data consumption

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import './App.css'
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { list: list}
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID ==! id
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({list: updatedList})
+    }
+  render() {
+    return (
+      <div className="App">
+        { this.state.list.map( item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{ item.title }</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                  Dismiss
+                </button>
+              </span>
+            </div>
+        )}
+      </div>
+    );
+  }
+
+  }
+
+
+export default App
